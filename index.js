0669e1fb938b580e0a1bdee98932ec84fa7bb961
ReubG <reubengreening@gmail.com>
2017-06-29 19:47:24 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const API = require('./api-functions');
 const config = require('./config');
+const express    = require('express');
+const app        = express();
 
+app.set('port', (process.env.PORT || 5000));
+
+app.get('/', function(request, response) {
+    var result = 'App is running'
+    response.send(result);
+}).listen(app.get('port'), function() {
+    console.log('App is running, server is listening on port ', app.get('port'));
+});
 /** @class ContestJSBot */
 class ContestJSBot {
 
