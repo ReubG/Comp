4068ae91fc880c9b4dcfa9ec2e6d68ea15e268f5
ReubG <reubengreening@gmail.com>
2017-06-29 23:16:15 +0100

    Update config.js

--- config.js
+++ config.js
@@ -10,6 +10,10 @@ const config = {
 
     // The searches the bot will do
     SEARCH_QUERIES: [
+        'retweet to win',
+        'RT to win',
+        'retweet 2 win',
+        'RT 2 win',
         '#COMPETITION',
         '#giveaway'
        
